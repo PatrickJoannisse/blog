46b92777a8ea93d54e8979cdf28cca252bc73cc5
Patrick Joannisse <patrick.joannisse@hey.com>
2022-03-03 15:12:32 -0500

    Creating default layout

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="dark:bg-gray-900">
+    <SkipLink />
+    <TopNav />
+    <section class="max-w-screen-lg mx-auto px-4 py-4" id="content">
+      <Nuxt />
+    </section>
+    <Footer />
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    bodyAttrs: {
+      class: "dark:bg-[#0e0e0e]",
+    },
+  },
+};
+</script>
