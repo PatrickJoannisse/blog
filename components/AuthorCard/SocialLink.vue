173ad94de7a7d474b208ce601f6d47831445c6fe
Patrick Joannisse <patrick.joannisse@hey.com>
2022-03-03 22:20:59 -0500

    Improve AuthorCard and create SocialLink component

---
+++ components/AuthorCard/SocialLink.vue
@@ -0,0 +1,16 @@
+<template>
+  <a
+    :href="this.href"
+    class="inline-flex items-center justify-center w-full px-3 py-2 space-x-4 text-center text-white transition duration-150 bg-gray-700  md:text-gray-500 md:bg-transparent md:w-auto hover:text-gray-100"
+  >
+    <slot></slot>
+  </a>
+</template>
+<script>
+export default {
+  name: "SocialLink",
+  props: {
+    href: String,
+  },
+};
+</script>
--- pages/posts/_slug.vue
+++ pages/posts/_slug.vue
@@ -1,15 +1,15 @@
 <template>
   <section>
-    <header class="max-w-screen-lg mx-auto px-4 py-4">
-      <h1 class="text-5xl font-extrabold dark:text-gray-100">
+    <header class="max-w-screen-lg px-4 py-4 mx-auto">
+      <h1 class="text-3xl font-extrabold md:text-5xl dark:text-gray-100">
         {{ article.title }}
       </h1>
-      <div class="text-sm my-4 flex items-center space-x-6">
+      <div class="flex items-center my-4 space-x-6 text-sm">
         <span class="dark:text-gray-400">{{
           formatDate(article.updatedAt)
         }}</span>
         <p class="sr-only">Tags:</p>
-        <ul class="inline-flex space-x-2 my-2">
+        <ul class="inline-flex my-2 space-x-2">
           <li v-for="tag in article.tags" :key="tag">
             <NuxtLink
               :to="`/tags/${tag}/`"
@@ -37,31 +37,20 @@
       v-if="article.img"
       :src="article.img"
       :alt="article.alt"
-      class="xl:max-w-screen-xl mx-auto"
+      class="mx-auto xl:max-w-screen-xl"
     />
     <p
-      class="max-w-screen-xl mx-auto dark:text-gray-600 text-center mt-1 italic"
+      class="max-w-screen-xl mx-auto mt-1 italic text-center dark:text-gray-600"
     >
       {{ article.alt }}
     </p>
     <article
-      class="
-        prose
-        lg:prose-lg
-        dark:prose-invert
-        dark:prose-a:text-indigo-400
-        dark:hover:prose-a:text-indigo-500
-        prose-a:transition prose-a:duration-150
-        max-w-screen-lg
-        mx-auto
-        mt-6
-        px-4
-      "
+      class="max-w-screen-lg px-4 mx-auto mt-6 prose  md:prose-md lg:prose-lg dark:prose-invert dark:prose-a:text-indigo-400 dark:hover:prose-a:text-indigo-500 prose-a:transition prose-a:duration-150"
     >
       <p class="lead">{{ article.description }}</p>
       <nuxt-content :document="article" />
     </article>
-    <div class="max-w-screen-lg mx-auto px-4 my-6" id="content">
+    <div class="max-w-screen-lg px-4 mx-auto my-6" id="content">
       <AuthorCard />
     </div>
   </section>
