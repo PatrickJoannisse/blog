e3b74654a2c5b6c0da48cd7d2040c5a35254b242
Patrick Joannisse <patrick.joannisse@hey.com>
2022-04-29 18:40:35 -0400

    Add LinkedIn link

--- components/AuthorCard/AuthorCard.vue
+++ components/AuthorCard/AuthorCard.vue
@@ -6,7 +6,7 @@
       <img
         src="~/assets/img/me.png"
         class="w-40 mx-auto rounded-full ring-gray-100 ring-2"
-        alt="Photo of me"
+        alt="Hey, that's a photo of me"
       />
     </div>
     <div class="md:w-5/6">
@@ -16,7 +16,7 @@
         Hi! I'm Patrick Joannisse
       </h2>
       <p class="text-center dark:text-gray-400 md:text-left">
-        I'm a Full Stack web developer specialized in web accessibility. Lorem
+        I'm a senior software developer, public servant and overall geek. Lorem
         ipsum dolor, sit amet consectetur adipisicing elit. Aliquid iusto fugit
         quo consequatur atque optio, libero eveniet perferendis officiis magni
         omnis quae dignissimos esse, voluptatem at molestiae in! Cum, debitis!
@@ -49,6 +49,21 @@
             />
           </svg>
           <span class="md:sr-only">Github</span>
+        </AuthorCardSocialLink>
+        <AuthorCardSocialLink href="http://linkedin.com/in/patrick-joannisse">
+          <svg
+            class="w-5 h-5 mt-0.5"
+            viewBox="0 0 15 15"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M2 1C1.44772 1 1 1.44772 1 2V13C1 13.5523 1.44772 14 2 14H13C13.5523 14 14 13.5523 14 13V2C14 1.44772 13.5523 1 13 1H2ZM3.05 6H4.95V12H3.05V6ZM5.075 4.005C5.075 4.59871 4.59371 5.08 4 5.08C3.4063 5.08 2.925 4.59871 2.925 4.005C2.925 3.41129 3.4063 2.93 4 2.93C4.59371 2.93 5.075 3.41129 5.075 4.005ZM12 8.35713C12 6.55208 10.8334 5.85033 9.67449 5.85033C9.29502 5.83163 8.91721 5.91119 8.57874 6.08107C8.32172 6.21007 8.05265 6.50523 7.84516 7.01853H7.79179V6.00044H6V12.0047H7.90616V8.8112C7.8786 8.48413 7.98327 8.06142 8.19741 7.80987C8.41156 7.55832 8.71789 7.49825 8.95015 7.46774H9.02258C9.62874 7.46774 10.0786 7.84301 10.0786 8.78868V12.0047H11.9847L12 8.35713Z"
+              fill="currentColor"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
         </AuthorCardSocialLink>
       </div>
     </div>
