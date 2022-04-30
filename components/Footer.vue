68bea495fe6c4e528534992a0e0a2ef97d7e3cc1
Patrick Joannisse <patrick.joannisse@hey.com>
2022-04-30 10:41:18 -0400

    Update links

--- components/Footer.vue
+++ components/Footer.vue
@@ -5,9 +5,9 @@
       class="max-w-screen-lg px-4 py-6 mx-auto  sm:px-6 md:flex md:items-center md:justify-between"
     >
       <div class="flex justify-center space-x-6 md:order-2">
-        <NuxtLink
+        <!-- <NuxtLink
           to="/"
-          class="inline-flex items-center space-x-2 text-gray-700  hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-100"
+          class="inline-flex items-center space-x-2 text-gray-700 hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-100"
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -24,9 +24,9 @@
             />
           </svg>
           <span class="sr-only">RSS</span>
-        </NuxtLink>
+        </NuxtLink> -->
         <a
-          href="#"
+          href="https://twitter.com/MrJoannisse"
           class="text-gray-700  hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-100"
         >
           <span class="sr-only">Twitter</span>
@@ -43,7 +43,26 @@
         </a>
 
         <a
-          href="#"
+          href="https://www.linkedin.com/in/patrick-joannisse/"
+          class="text-gray-700  hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-100"
+        >
+          <span class="sr-only">LinkedIn</span>
+          <svg
+            class="w-6 h-6"
+            fill="currentColor"
+            viewBox="0 0 15 15"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M2 1C1.44772 1 1 1.44772 1 2V13C1 13.5523 1.44772 14 2 14H13C13.5523 14 14 13.5523 14 13V2C14 1.44772 13.5523 1 13 1H2ZM3.05 6H4.95V12H3.05V6ZM5.075 4.005C5.075 4.59871 4.59371 5.08 4 5.08C3.4063 5.08 2.925 4.59871 2.925 4.005C2.925 3.41129 3.4063 2.93 4 2.93C4.59371 2.93 5.075 3.41129 5.075 4.005ZM12 8.35713C12 6.55208 10.8334 5.85033 9.67449 5.85033C9.29502 5.83163 8.91721 5.91119 8.57874 6.08107C8.32172 6.21007 8.05265 6.50523 7.84516 7.01853H7.79179V6.00044H6V12.0047H7.90616V8.8112C7.8786 8.48413 7.98327 8.06142 8.19741 7.80987C8.41156 7.55832 8.71789 7.49825 8.95015 7.46774H9.02258C9.62874 7.46774 10.0786 7.84301 10.0786 8.78868V12.0047H11.9847L12 8.35713Z"
+              fill="currentColor"
+              fill-rule="evenodd"
+              clip-rule="evenodd"
+            ></path>
+          </svg>
+        </a>
+        <a
+          href="https://github.com/PatrickJoannisse"
           class="text-gray-700  hover:text-gray-600 dark:text-gray-400 dark:hover:text-gray-100"
         >
           <span class="sr-only">GitHub</span>
@@ -63,7 +82,7 @@
       </div>
       <div class="mt-8 md:mt-0 md:order-1">
         <p class="text-base text-center text-dark-700 dark:text-gray-400">
-          &copy; 2022 Red Tape &amp; Dragons.
+          &copy; 2022 Patrick Joannisse
         </p>
       </div>
     </div>
